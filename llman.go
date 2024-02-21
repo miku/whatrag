a7ce04aca5ac5ddee949e9bf757df9c8ba76b6f9
Martin Czygan <martin.czygan@gmail.com>
2024-02-21 13:33:22 +0100

    llman

--- llman.go
+++ llman.go
@@ -6,22 +6,21 @@ import (
 	"log"
 
 	"github.com/henomis/lingoose/llm/ollama"
-	"github.com/henomis/lingoose/llm/openai"
 	"github.com/henomis/lingoose/thread"
 )
 
 func main() {
 	myThread := thread.New().AddMessage(
 		thread.NewUserMessage().AddContent(
-			thread.NewTextContent("Tell me a joke about geese"),
+			thread.NewTextContent("hello world in haskell"),
 		),
 	)
-	err := ollama.New().WithEndpoint("http://localhost:11434/api").WithModel("mistral").
+	err := ollama.New().WithEndpoint("http://localhost:11434/api").WithModel("mistral:instruct").
 		WithStream(func(s string) {
+			fmt.Print(s)
 		}).Generate(context.Background(), myThread)
-	err := openai.New().Generate(context.Background(), myThread)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(myThread)
+	// fmt.Println(myThread)
 }
